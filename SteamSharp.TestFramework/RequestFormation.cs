79668143bd3dda59ccba632404eab017be92a518
Shane Chism <hey@shanechism.com>
2013-11-28 12:06:01 -0800

    Establish structure, partial buildout of Rest transaction classes.

---
+++ SteamSharp.TestFramework/RequestFormation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SteamSharp.TestFramework {
+	
+	public class RequestFormation {
+
+		[Fact]
+		public void Creation_Of_Client() {
+
+			SteamClient client = new SteamClient();
+
+			Assert.NotNull( client );
+			Assert.NotNull( client.AssemblyVersion );
+
+		}
+
+		public void Creation_Of_Request() {
+
+
+
+		}
+
+	}
+
+}
